4216175fbeebebd88b7165bd99b8d9db66130ac8
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-09-05 03:39:42 -0400

    got colored square to appear again. Need to review texture next

--- gltut4/main.c
+++ gltut4/main.c
@@ -20,12 +20,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void gl_sanity_test(void);
+
 int main(int argc, const char * argv[]) {
     
+    GLFWwindow *window;
+    GLuint vao;
+    GLuint vbo;
+    GLuint ebo;
+    GLuint program;
     
     
-    initShaderFiles("vshader.vsh", "fshader.fsh");
+    GLfloat square[] = {
+        //Position          //Color
+        -0.5, -0.5, 0.0,    1.0, 0.0, 0.0,
+        -0.5,  0.5, 0.0,    0.0, 1.0, 0.0,
+         0.5, -0.5, 0.0,    0.0, 0.0, 1.0,
+         0.5,  0.5, 0.0,    0.0, 0.0, 0.0,
+    };
+    
+    GLuint index[] = {
+        0, 1, 2,
+        1, 2 ,3,
+    };
+    
+    if(!glfwInit()){
+        printf("GLFW could not initialize.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
+    
+    window = glfwCreateWindow(400, 400, "Transformation", NULL, NULL);
+    
+    if(!window){
+        printf("Window could not open.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        printf("GLAD failed to load.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    //
     // insert code here...
-    printf("Hello, World!\n");
+    program = initShaderFiles("vshader.vsh", "fshader.fsh");
+    
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(square), square, GL_STATIC_DRAW);
+    printf("2\n");
+    gl_sanity_test();
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+    printf("1\n");
+    gl_sanity_test();
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    printf("3\n");
+    gl_sanity_test();
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glUseProgram(program);
+    while (!glfwWindowShouldClose(window)) {
+        
+        glClearColor(1.0,1.0,1.0,1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
+}
+
+void gl_sanity_test(){
+    GLenum err;
+    while((err = glGetError())!= GL_NO_ERROR){
+        printf("Something broke. OpenGL err: %x\n", err);
+    }
 }
--- gltut4/vshader.vsh
+++ gltut4/vshader.vsh
@@ -1,1 +1,14 @@
-this is a test for vertex shader.
+#version 330
+
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+//layout(location = 2) in vec2 aTexCoord;
+
+out vec3 myColor;
+//out vec2 myTexCoord;
+
+void main(){
+    gl_Position = vec4(aPos, 1.0);
+    myColor = aColor;
+    //myTexCoord = aTexCoord;
+}
