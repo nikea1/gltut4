37b13ab046a50293c2c22ec162f4117afaa420e1
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-09-05 02:38:11 -0400

    shader header source can read in shader files from outside. need to finish shader program.

--- gltut4/main.c
+++ gltut4/main.c
@@ -5,10 +5,26 @@
 //  Created by Adenike Adeyemi on 9/3/18.
 //  Copyright © 2018 Adenike Adeyemi. All rights reserved.
 //
+/**/
+#define GLFW_INCLUDE_GLCOREARB
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+//#include <glmc.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "shader.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
+    
+    
+    
+    initShaderFiles("vshader.vsh", "fshader.fsh");
     // insert code here...
     printf("Hello, World!\n");
     return 0;
